c6803f62133bcb5657c9e2e0728d364a4033548f
bobfaget <bobfaget@users.noreply.github.com>
2016-08-23 16:15:15 -0700

    Create backup.php

---
+++ backup/backup.php
@@ -0,0 +1,43 @@
+<?
+// Inspired by tutorials: http://www.phpfreaks.com/tutorials/130/6.php
+// http://www.vbulletin.com/forum/archive/index.php/t-113143.html
+// http://hudzilla.org
+
+
+// Create the mysql backup file
+// edit this section
+$dbhost = "sql2.hostinger.in"; // usually localhost
+$dbuser = "u868558704_ww";
+$dbpass = "y8vztaqu";
+$dbname = "u868558704_ww";
+$sendto = "Webmaster <bobfaget@tired.nice.urca.tv>";
+$sendfrom = "Automated Backup <backup@bobfaget.890m.com>";
+$sendsubject = "Daily Mysql Backup";
+$bodyofemail = "Here is the daily backup.";
+// don't need to edit below this section
+
+$backupfile = $dbname . date("Y-m-d") . '.sql';
+system("mysqldump -h $dbhost -u $dbuser -p$dbpass $dbname > $backupfile");
+
+
+// Mail the file
+
+
+include('Mail.php');
+include('Mail/mime.php');
+
+
+$message = new Mail_mime();
+$text = "$bodyofemail";
+$message->setTXTBody($text);
+$message->AddAttachment($backupfile);
+$body = $message->get();
+$extraheaders = array("From"=>"$sendfrom", "Subject"=>"$sendsubject");
+$headers = $message->headers($extraheaders);
+$mail = Mail::factory("mail");
+$mail->send("$sendto", $headers, $body);
+
+
+// Delete the file from your server
+unlink($backupfile);
+?>
