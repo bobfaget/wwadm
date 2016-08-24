bab796592e3a1cb7b398b1368fbdbbf0d176d441
bobfaget <bobfaget@users.noreply.github.com>
2016-08-23 19:36:40 -0700

    Update export.php

--- backup/export.php
+++ backup/export.php
@@ -76,26 +76,9 @@
         }
         //$backup_name = $backup_name ? $backup_name : $name."___(".date('H-i-s')."_".date('d-m-Y').")__rand".rand(1,11111111).".sql";
         $backup_name = $backup_name ? $backup_name : $name."_".date("Y-m-d").".sql";
-        //header('Content-Type: application/octet-stream');   
-        //header("Content-Transfer-Encoding: Binary"); 
-        //header("Content-disposition: attachment; filename=\"".$backup_name."\"");  
-        //echo $content; exit;
-$api_dev_key = 'c38ec99f32ced18f44d5587bba190128'; // your api_developer_key 
-$api_paste_code = "/* DB Backup ".date("Y-m-d h:i:s")." - This Data Was Automatically Generated */ \n".$content; // your paste text, Add NEW LINES FOR New Lines, It will be replaced!!! 
-$api_paste_private = '1'; // 0=public 1=unlisted 2=private 
-$api_paste_name = $name.'_'.date("Y-m-d h.i.s").".sql"; // name or title of your paste 
-$api_paste_expire_date = 'N'; 
-$api_paste_format = 'text'; 
-$api_user_key = 'c6c1b832aa50ae41590872bed055ab98'; // if an invalid api_user_key or no key is used, the paste will be create as a guest 
-$api_paste_name = urlencode($api_paste_name); 
-$api_paste_code = urlencode($api_paste_code); 
-$api_paste_code = str_replace("%5Cn", "%0A", $api_paste_code);
-$url = 'http://pastebin.com/api/api_post.php'; 
-$ch = curl_init($url); curl_setopt($ch, CURLOPT_POST, true); 
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'api_option=paste&api_user_key='.$api_user_key.'&api_paste_private='.$api_paste_private.'&api_paste_name='.$api_paste_name.'&api_paste_expire_date='.$api_paste_expire_date.'&api_paste_format='.$api_paste_format.'&api_dev_key='.$api_dev_key.'&api_paste_code='.$api_paste_code.''); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_VERBOSE, 1); 
-curl_setopt($ch, CURLOPT_NOBODY, 0); $response = curl_exec($ch);
-echo $response;
+        header('Content-Type: application/octet-stream');   
+        header("Content-Transfer-Encoding: Binary"); 
+        header("Content-disposition: attachment; filename=\"".$backup_name."\"");  
+        echo $content; exit;
     }
 ?>
